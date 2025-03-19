48ee25e1a998e2bf84f5ba342ee07b58a8aa6f7b
Eugen <belexiue@gmail.com>
2025-03-19 15:36:52 +0200

    My first commit

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("My Love")
+                .font(.headline)
+                .fontWeight(.semibold)
         }
         .padding()
     }
