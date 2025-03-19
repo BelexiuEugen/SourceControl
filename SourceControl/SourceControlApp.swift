ecda79a65ad8390fcce6fcdfb8e4519e66c41d86
Eugen <belexiue@gmail.com>
2025-03-19 14:26:46 +0200

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Jan on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
