099dd538ff654cf82a023bc25fbd270d037c5163
Eugen <belexiue@gmail.com>
2025-03-19 15:45:09 +0200

    Home view finnished

---
+++ SourceControl/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  SourceControl
+//
+//  Created by Jan on 19/03/2025.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Just some random text here for being straight.")
+    }
+}
+
+#Preview {
+    SecondView()
+}
