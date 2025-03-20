43ba36f19406cfd15d307c5439314472eccbfb0a
Eugen <belexiue@gmail.com>
2025-03-20 14:15:11 +0200

    [Feature] add something new

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -5,6 +5,25 @@
 //  Created by Jan on 19/03/2025.
 //
 
+/*
+ 
+ NEW FEATURRE :
+ [Feature] description of the feature
+ 
+ BUG in PRODUCTION
+ [PATCH] description of patch
+ 
+ BUG NOT in PRODUCTION
+ [bug] description of bug
+ 
+ Mundane TASKS:
+ [Clean] Description of changes
+ 
+ Release:
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
