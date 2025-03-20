03ad637ad22ea74b17adbc4913e634c2e8d74c1f
Eugen <belexiue@gmail.com>
2025-03-20 17:31:25 +0200

    [Feature] just changing the system name of the image

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -37,7 +37,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "house.fill")
+            Image(systemName: "magnifingglasss")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("My Love haah")
